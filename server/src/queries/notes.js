42b050b46e11903bd165a03b120bb0533307205a
Zach Jones <zzachjoness@gmail.com>
2021-08-09 14:06:32 -0400

    clean notes.js

--- server/src/queries/notes.js
+++ server/src/queries/notes.js
@@ -9,38 +9,41 @@ const pool = new Pool({
 });
 
 const getNotes = async (req, res) => {
-	if (!req.session.userID) {
-		return res.send({
+	const userID = req.session.userID;
+	if (!userID) {
+		return res.status(400).send({
 			errors: [{ field: "me", message: "user not logged in" }],
 		});
 	}
+	const text = "SELECT * FROM notes WHERE uid = $1";
+	const values = [userID];
 	try {
-		const result = await pool.query("SELECT * FROM notes WHERE uid = $1", [
-			req.session.userID,
-		]);
+		const result = await pool.query(text, values);
 		const notes = result.rows;
 		return res.status(200).send({ notes });
 	} catch (err) {
-		return res.send({ error: err });
+		return res.status(400).send({ error: err });
 	}
 };
 
 const createNote = async (req, res) => {
-	const result = await pool.query(
-		`INSERT INTO notes(uid, last_updated) VALUES ($1, (to_timestamp(${Date.now()} / 1000.0))) RETURNING *`,
-		[req.session.userID]
-	);
-	const note = result.rows;
-	res.status(200).send({ note });
+	const text = `INSERT INTO notes(uid, last_updated) VALUES ($1, (to_timestamp(${Date.now()} / 1000.0))) RETURNING *`;
+	const values = [req.session.userID];
+	try {
+		const result = await pool.query(text, values);
+		const note = result.rows;
+		return res.status(200).send({ note });
+	} catch (err) {
+		return res.status(400).send({ error: err });
+	}
 };
 
 const deleteNote = async (req, res) => {
 	const nid = req.body.nid;
+	const text = `DELETE FROM notes WHERE nid = $1 RETURNING *`;
+	vaules = [nid];
 	try {
-		const result = await pool.query(
-			`DELETE FROM notes WHERE nid = $1 RETURNING *`,
-			[nid]
-		);
+		const result = pool.query(text, values);
 		const notes = result.rows;
 		console.log("returning notes: ", notes);
 		return res.send({ notes });
@@ -52,13 +55,12 @@ const deleteNote = async (req, res) => {
 
 const updateNote = async (req, res) => {
 	const { category, title, body, nid } = req.body;
-	try {
-		const result = await pool.query(
-			`UPDATE notes SET (category, title, body, last_updated) 
+	const text = `UPDATE notes SET (category, title, body, last_updated) 
 			= ($1, $2, $3, (to_timestamp(${Date.now()} / 1000.0))) 
-			WHERE nid = $4 RETURNING *`,
-			[category, title, body, nid]
-		);
+			WHERE nid = $4 RETURNING *`;
+	const values = [category, title, body, nid];
+	try {
+		const result = await pool.query(text, values);
 		const note = result.rows;
 		return res.status(200).send({ note });
 	} catch (err) {
